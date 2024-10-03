417192128d2d441591b479dc99ceb1ce04874658
Jayandra Hamal <salonhamal278@gmail.com>
2024-10-03 22:57:04 +0545

    creating api for products

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,27 @@
 import express from 'express';
 import dotenv from "dotenv"
-import { connectDB } from './config/db.js';
+import mongoose from'mongoose';
+
+import productRoutes from './routes/product.route.js'
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
-app.get("/" , (req,res) => {
-  res.send("server is ready")
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+  console.log("MongoDB Connected");
+})
+.catch((err)=> {
+  console.error(err.message);
+  process.exit(1);
 })
 
 app.listen(5000, () => {
-  connectDB();
   console.log('Server is running on port 5000');
 })
+
+app.use("/api/products", productRoutes)
+
