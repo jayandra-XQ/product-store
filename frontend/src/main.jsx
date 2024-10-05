b1b9124edef41c4bddbfaec3a7a3136e35bdb17d
Jayandra Hamal <salonhamal278@gmail.com>
2024-10-05 18:42:43 +0545

    build navbar and also toggle theme dark mode light mode using chakra ui

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { ChakraProvider } from '@chakra-ui/react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import App from './App.jsx'
-import './index.css'
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ChakraProvider>
-      <App />
-    </ChakraProvider>
+    <BrowserRouter>
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
